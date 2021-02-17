e00ea0095e6f3f18c95199d40850f77db5ff3556
Antonio MaquinaVirtual <asanchez@visuald.cl>
2021-02-17 12:45:34 -0300

    creacion de actividad desde portal, con todos los campos a excepcion de Activity Subject revisa proximo commit

---
+++ Docs/scriptTablasRegistroActividad.sql
@@ -0,0 +1,11 @@
+
+SELECT * FROM OCLG -- Actividad
+
+SELECT * FROM OCLT -- Tipo de Actividad
+
+SELECT * FROM OCLS -- Asuto (drill down del tipo de actividad)
+
+
+-- conectarme a una BD de SAP y desde alli hacer la consulta
+
+
